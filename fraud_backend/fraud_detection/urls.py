71c6119bb058c85fb1d0b7780c0f49ca78ecd821
Adel JDA <24362949+edsteine@users.noreply.github.com>
2024-10-21 16:37:47 +0100

    Update urls.py

--- fraud_backend/fraud_detection/urls.py
+++ fraud_backend/fraud_detection/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views.auth_views import UserRegistrationView, LogoutView, LoginView, UserProfileView
 from .views.api_views import TransactionListView, TransactionDetailView, TransactionCreateView, TransactionProcessView, CheckFraudView
+from .views.HomeView import HomeView  # Import HomeView here
 
 urlpatterns = [
+    # Home view
+    path('', HomeView.as_view(), name='home'),  # Add this line for the home view
+
     # User authentication
     path('register/', UserRegistrationView.as_view(), name='user_registration'),
     path('login/', LoginView.as_view(), name='login'),
@@ -10,18 +14,9 @@ urlpatterns = [
     path('profile/', UserProfileView.as_view(), name='user_profile'),
 
     # URL patterns for transaction management
-    # Route for listing all transactions
     path('transactions/', TransactionListView.as_view(), name='transaction_list'),
-
-    # Route for creating a new transaction
     path('transactions/create/', TransactionCreateView.as_view(), name='transaction_create'),
-
-    # Route for viewing details of a specific transaction
     path('transactions/<int:pk>/', TransactionDetailView.as_view(), name='transaction_detail'),
-
-    # Route for processing a specific transaction
     path('transactions/<int:pk>/process/', TransactionProcessView.as_view(), name='transaction_process'),
-
-    # Route for checking fraud on a specific transaction
     path('transactions/<int:pk>/check_fraud/', CheckFraudView.as_view(), name='check_fraud'),
 ]
